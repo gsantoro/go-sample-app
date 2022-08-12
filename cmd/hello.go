f1a927e88ebee63c8e9fba00403f304ba87a25a1
gsantoro <giuseppe.santoro@gmail.com>
2022-08-12 18:48:23 +0100

    sample golanci.yml from internet, sample gin endpoint + logs, taskfile command to run hot reload

--- cmd/hello.go
+++ cmd/hello.go
@@ -3,17 +3,28 @@ package main
 import (
 	"os"
 
+	"net/http"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	logger := log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	sublogger := log.With().
-		Str("hostname", "localhost").
-		Logger()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		logger.Debug().
+			Str("method", c.Request.Method).
+			Str("endpoint", c.Request.RequestURI).
+			Msg("Pong")
 
-	sublogger.Info().Msg("hello world")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
