97e9032c66dfa7e2463d55dcbafcbf266b7e6495
gsantoro <giuseppe.santoro@gmail.com>
2022-08-15 18:31:41 +0100

    cleanup + support for prometheus

--- cmd/hello.go
+++ cmd/hello.go
@@ -1,42 +1,47 @@
 package main
 
 import (
-	"os"
-
 	"net/http"
+	"os"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zsais/go-gin-prometheus"
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	logger := log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		logger.Debug().
-			Str("method", c.Request.Method).
-			Str("endpoint", c.Request.RequestURI).
-			Msg("Pong")
+	r := gin.Default()
+	r.GET("/ping", Ping)
 
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(r)
 
-	err := router.SetTrustedProxies(nil)
+	err := r.SetTrustedProxies(nil)
 	if err != nil {
 		logger.Error().Err(err)
 	}
 
 	addr := "0.0.0.0:8080"
-	err = router.Run(addr)
+	err = r.Run(addr)
 	if err != nil {
 		logger.Error().
 			Str("endpoint", addr).
 			Msg("Cannot start server")
 	}
+}
+
+func Ping(c *gin.Context) {
+	// logger.Debug().
+	// 	Str("method", c.Request.Method).
+	// 	Str("endpoint", c.Request.RequestURI).
+	// 	Msg("Pong")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
