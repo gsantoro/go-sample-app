02140bfbd8156146fa507effd68ee2d687b444d6
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-09-02 17:47:44 +0200

    swagger mvp

--- internal/rest/health.go
+++ internal/rest/health.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Health        Used to check the health of the web api
+// @Summary      Health
+// @Description  Responds with status=ok.
+// @Tags         Health
+// @Produce      json
+// @Router       /health [get]
 func Health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "OK",
