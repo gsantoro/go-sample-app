fe703058da84b694b638082c9e9284addffddcb7
Giuseppe Santoro <giuseppe.santoro@gmail.com>
2023-05-08 19:23:40 +0100

    cdk8s sample

--- devenv.nix
+++ devenv.nix
@@ -20,6 +20,7 @@
     pkgs.kubernetes-helm
     pkgs.go-task
     pkgs.k9s
+    # pkgs.nodePackages.cdk8s-cli  #Note: Currently 2.2.25 while github at 2.2.29
     # pkgs.kube3d # Note: Currently at 5.4.4 while github at 5.4.9
   ];
 
